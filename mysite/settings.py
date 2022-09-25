d266394b9ff36bfc362fcfbcf936e569cd2d887c
rthway <rthway@gmail.com>
2022-09-25 15:21:04 +0545

    fix NameError: name TEMPLATE_DIRS is not defined. Did you mean: TEMPLATES_DIR?

--- mysite/settings.py
+++ mysite/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIRS],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
