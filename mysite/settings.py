6f4a8b509b6ad2793c4ad6eb933912256209535c
rthway <rthway@gmail.com>
2022-09-25 15:27:07 +0545

    urls.py file add inside blog dir

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,9 +80,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
